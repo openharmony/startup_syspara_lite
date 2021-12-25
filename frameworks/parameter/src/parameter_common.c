9d4f65fcfae80e4c11ba52abf7a4ef135151f380
熊磊 <xionglei6@huawei.com>
2021-12-25 06:38:41 +0000

    修改轻鸿蒙3.1beta版本号 Signed-off-by: xionglei <xionglei6@huawei.com>

--- frameworks/parameter/src/parameter_common.c
+++ frameworks/parameter/src/parameter_common.c
@@ -37,13 +37,13 @@
 #define OHOS_PATCH_VERSION_FILE "/patch/pversion"
 
 static const char OHOS_OS_NAME[] = { "OpenHarmony" };
-static const int OHOS_SDK_API_VERSION = 6;
+static const int OHOS_SDK_API_VERSION = 8;
 static const char OHOS_SECURITY_PATCH_TAG[] = {"2021-09-01"};
 static const char OHOS_RELEASE_TYPE[] = { "Beta" };
 
-static const int MAJOR_VERSION = 1;
-static const int SENIOR_VERSION = 0;
-static const int FEATURE_VERSION = 1;
+static const int MAJOR_VERSION = 3;
+static const int SENIOR_VERSION = 1;
+static const int FEATURE_VERSION = 0;
 static const int BUILD_VERSION = 0;
 
 static const char EMPTY_STR[] = { "" };
