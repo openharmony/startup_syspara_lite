947e8a8d96182d283396c2004ba5ab606b257ba1
honghao534 <honghao534@163.com>
2023-07-31 17:39:56 +0800

    update security patch level to 3.0LTS
    Signed-off-by: honghao534 <honghao534@163.com>

--- interfaces/innerkits/native/syspara/src/parameter.c
+++ interfaces/innerkits/native/syspara/src/parameter.c
@@ -30,7 +30,7 @@
 static const char OHOS_OS_NAME[] = {"OpenHarmony"};
 static const int OHOS_SDK_API_LEVEL = 6;
 static const char OHOS_BUILD_ROOT_HASH[] = {"****"};
-static const char OHOS_SECURITY_PATCH_TAG[] = {"2023/07/01"};
+static const char OHOS_SECURITY_PATCH_TAG[] = {"2023/08/01"};
 static const char OHOS_RELEASE_TYPE[] = {"LTS"};
 static const int DEV_BUF_LENGTH = 3;
 static const int DEV_BUF_MAX_LENGTH = 1024;
